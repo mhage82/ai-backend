1712816c0cced4a025a533dab751f01849d1c951
Mohammad Hassan <WZ2W97@NAM.CORP.GM.COM>
2025-07-14 05:31:04 -0400

    load unsolved

--- search/app.py
+++ search/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify, send_from_directory, send_file
 from flask_cors import CORS
 import os
 from maze_solver import Maze
